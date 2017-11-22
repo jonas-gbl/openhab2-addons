3949b3c1a8f0966767de628d52d646d1d36e114c
Jonas Gabriel <jga@unwire.dk>
2017-11-22 15:02:57 +0100

    [VS-001] Created basic thing definition

---
+++ addons/binding/org.openhab.binding.verisure/src/main/java/org/openhab/binding/verisure/VerisureBindingConstants.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.verisure;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link VerisureBindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Jonas Gabriel - Initial contribution
+ */
+@NonNullByDefault
+public class VerisureBindingConstants {
+
+
+    // List of all Channel ids
+    public static final String ARM_DISARM_CHANNEL = "arm-disarm-channel";
+    public static final String ALARM_STATUS_CHANNEL = "alarm-status-channel";
+    private static final String BINDING_ID = "verisure";
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_VERISURE_ALARM = new ThingTypeUID(BINDING_ID, "verisure-alarm");
+
+}
