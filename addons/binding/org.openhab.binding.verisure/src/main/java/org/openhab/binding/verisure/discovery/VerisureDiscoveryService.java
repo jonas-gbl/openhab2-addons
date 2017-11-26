3b06fde5d37b8833da09a9c2ebff037ba7309c91
Jonas Gabriel <jga@unwire.dk>
2017-11-26 21:13:05 +0100

    [VS-018] Just added skeleton for VerisureDiscoveryService

---
+++ addons/binding/org.openhab.binding.verisure/src/main/java/org/openhab/binding/verisure/discovery/VerisureDiscoveryService.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.verisure.discovery;
+
+import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
+
+/**
+ * The {@link VerisureDiscoveryService} is used to connect to the Verisure services.
+ *
+ * @author Jonas Gabriel - Initial contribution
+ */
+public class VerisureDiscoveryService extends AbstractDiscoveryService {
+
+    public VerisureDiscoveryService(int timeout) throws IllegalArgumentException {
+        super(timeout);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void startScan() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
